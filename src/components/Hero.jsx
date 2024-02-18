6fffd6ad70652315f9cb5137d528fa53857adf8d
Sanika Patil <2611sanika@gmail.com>
2024-02-18 20:31:08 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <h2 className="w-28 object-contain">Artisum</h2>
 
         <button
           type='button'
@@ -21,12 +21,9 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='text-blue-900 '>OpenAI GPT-4</span>
       </h1>
-      <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
-      </h2>
+      
     </header>
   );
 };
